c7d5d75da33e502d3ad31c16312009fad80464f4
Irham Dzuhri <irhamdz@gmail.com>
2024-08-15 09:55:42 +0700

    add func for coloring role

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta, timezone
 
+
 def convert_datetime_str_to_utc(datetime_str):
     decimal_index = datetime_str.find(".")
     if decimal_index != -1:
@@ -12,13 +13,13 @@ def convert_datetime_str_to_utc(datetime_str):
 def format_duration(seconds):
     message = ""
     duration_td = timedelta(seconds=seconds)
-    days , day_remainder = divmod(duration_td.total_seconds(), 86400)
+    days, day_remainder = divmod(duration_td.total_seconds(), 86400)
     hours, remainder = divmod(day_remainder, 3600)
     minutes, seconds = divmod(remainder, 60)
 
     if days:
         message = f"{int(days)} hari "
-    
+
     if hours:
         message = message + f"{int(hours)} jam "
 
@@ -34,3 +35,12 @@ def remove_query_id_from_tg_web_data(tg_web_data):
     data_to_be_splitted = tg_web_data
     splitted_original_data = data_to_be_splitted.split("&")
     return "&".join(splitted_original_data[1:])
+
+
+def mapping_role_color(role):
+    if role == "admin":
+        role = f"<lg>{role}</lg>"
+    elif role == "premium":
+        role = f"<lc>{role}</lc>"
+
+    return role
