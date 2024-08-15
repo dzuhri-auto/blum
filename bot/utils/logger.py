84d9e80de894e1495205db397004aae90e95153d
Irham Dzuhri <irhamdz@gmail.com>
2024-08-15 20:20:05 +0700

    improve balance visual

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -3,9 +3,8 @@ from loguru import logger
 
 
 logger.remove()
-logger.add(sink=sys.stdout, format="<white>{time:YYYY-MM-DD HH:mm:ss}</white>"
-                                   " | <level>{level}</level>"
-                                   " | <white><b>{message}</b></white>")
+log_format = "<white>{time:YYYY-MM-DD HH:mm:ss}</white> | <level>{level: <8}</level> | <cyan><b>{line}</b></cyan> - <white><b>{message}</b></white>"
+logger.add(sink=sys.stdout, format=log_format)
 logger = logger.opt(colors=True)
 
 
